e17cc2c4f84d53dcb881be7ea6db83dd0c5d7a82
Aaron Wang <66176554+aaron-wang@users.noreply.github.com>
2023-03-27 15:21:43 +0000

    Formatting.

--- service/src/main/java/bio/terra/axonserver/app/controller/SignedUrlController.java
+++ service/src/main/java/bio/terra/axonserver/app/controller/SignedUrlController.java
@@ -2,25 +2,14 @@ package bio.terra.axonserver.app.controller;
 
 import bio.terra.axonserver.api.SignedUrlApi;
 import bio.terra.axonserver.model.ApiSignedUrlReport;
-import bio.terra.axonserver.service.iam.SamService;
 import bio.terra.axonserver.service.signedurl.SignedUrlService;
 import bio.terra.axonserver.service.wsm.WorkspaceManagerService;
-import bio.terra.axonserver.utils.CloudStorageUtils;
 import bio.terra.common.exception.ApiException;
 import bio.terra.common.exception.BadRequestException;
 import bio.terra.common.iam.BearerToken;
 import bio.terra.common.iam.BearerTokenFactory;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ImpersonatedCredentials;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageException;
-import com.google.cloud.storage.StorageOptions;
 import java.io.IOException;
-import java.net.URL;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -60,7 +49,10 @@ public class SignedUrlController extends ControllerBase implements SignedUrlApi 
             .getGcpGcsBucket()
             .getBucketName();
     try {
-      String result = signedUrlService.generateV4GetObjectSignedUrl(token, projectId, bucketName, objectName).toString();
+      String result =
+          signedUrlService
+              .generateV4GetObjectSignedUrl(token, projectId, bucketName, objectName)
+              .toString();
       ApiSignedUrlReport actualResult = new ApiSignedUrlReport().signedUrl(result);
       return new ResponseEntity<>(actualResult, HttpStatus.OK);
     } catch (IOException e) {
--- service/src/main/java/bio/terra/axonserver/service/signedurl/SignedUrlService.java
+++ service/src/main/java/bio/terra/axonserver/service/signedurl/SignedUrlService.java
@@ -10,62 +10,63 @@ import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageException;
 import com.google.cloud.storage.StorageOptions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-
-/**
- * Service for generating signed URLs for Terra cloud resources.
- */
+/** Service for generating signed URLs for Terra cloud resources. */
 @Component
 public class SignedUrlService {
-    private final SamService samService;
+  private final SamService samService;
 
-    private static final int DEFAULT_SIGNED_URL_EXPIRATION_TIME_IN_MINUTES = 60;
+  private static final int DEFAULT_SIGNED_URL_EXPIRATION_TIME_IN_MINUTES = 60;
 
-    @Autowired
-    public SignedUrlService(
-        SamService samService
-    ) {
-        this.samService = samService;
-    }
+  @Autowired
+  public SignedUrlService(SamService samService) {
+    this.samService = samService;
+  }
 
-    /**
-     * Generate a V4 signed URL using the Google application default credentials and pet service
-     * account email.
-     *
-     * <p>See <a href="https://cloud.google.com/storage/docs/access-control/signed-urls">Signed
-     * URLs</a> and <a
-     * href="https://cloud.google.com/storage/docs/access-control/signing-urls-with-helpers#client-libraries">V4
-     * signing process with Cloud Storage tools</a>
-     */
-    public URL generateV4GetObjectSignedUrl(BearerToken token, String projectId, String bucketName, String objectName)
-        throws StorageException, IOException {
-        String petSaEmail = samService.getPetServiceAccount(projectId, token);
-        ImpersonatedCredentials targetCredentials =
-            ImpersonatedCredentials.create(
-                GoogleCredentials.getApplicationDefault(),
-                petSaEmail,
-                null,
-                CloudStorageUtils.getPetScopes(),
-                300);
+  /**
+   * Generate a V4 signed URL using the Google application default credentials and pet service
+   * account email.
+   *
+   * <p>See <a href="https://cloud.google.com/storage/docs/access-control/signed-urls">Signed
+   * URLs</a> and <a
+   * href="https://cloud.google.com/storage/docs/access-control/signing-urls-with-helpers#client-libraries">
+   * V4 signing process with Cloud Storage tools</a>
+   *
+   * @param token Bearer token for the requester.
+   * @param projectId The GCP project ID.
+   * @param bucketName Name of the GCS bucket.
+   * @param objectName Path to object in the bucket.
+   * @return A signed URL giving download access for one hour.
+   */
+  public URL generateV4GetObjectSignedUrl(
+      BearerToken token, String projectId, String bucketName, String objectName)
+      throws StorageException, IOException {
+    String petSaEmail = samService.getPetServiceAccount(projectId, token);
+    ImpersonatedCredentials targetCredentials =
+        ImpersonatedCredentials.create(
+            GoogleCredentials.getApplicationDefault(),
+            petSaEmail,
+            null,
+            CloudStorageUtils.getPetScopes(),
+            300);
 
-        Storage storage =
-            StorageOptions.newBuilder()
-                .setProjectId(projectId)
-                .setCredentials(targetCredentials)
-                .build()
-                .getService();
+    Storage storage =
+        StorageOptions.newBuilder()
+            .setProjectId(projectId)
+            .setCredentials(targetCredentials)
+            .build()
+            .getService();
 
-        BlobInfo blobInfo = BlobInfo.newBuilder(BlobId.of(bucketName, objectName)).build();
-        return storage.signUrl(
-            blobInfo,
-            DEFAULT_SIGNED_URL_EXPIRATION_TIME_IN_MINUTES,
-            TimeUnit.MINUTES,
-            Storage.SignUrlOption.withV4Signature());
-    }
+    BlobInfo blobInfo = BlobInfo.newBuilder(BlobId.of(bucketName, objectName)).build();
+    return storage.signUrl(
+        blobInfo,
+        DEFAULT_SIGNED_URL_EXPIRATION_TIME_IN_MINUTES,
+        TimeUnit.MINUTES,
+        Storage.SignUrlOption.withV4Signature());
+  }
 }
